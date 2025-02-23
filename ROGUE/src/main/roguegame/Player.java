6f60e6f6d5abdf4813d92d23b8fdff856889b736
karthikpinneboyina99 <157269250+karthikpinneboyina99@users.noreply.github.com>
2025-02-23 12:02:59 -0600

    Update Player.java

--- ROGUE/src/main/roguegame/Player.java
+++ ROGUE/src/main/roguegame/Player.java
@@ -1,6 +1,6 @@
-package main.roguegame;
-//test
-public class Player {
+package roguegame;
+
+class Player {
     private int x;
     private int y;
     private int health;
@@ -8,6 +8,8 @@ public class Player {
     private int gold;
     private int armor;
     private int food;
+    private int experience;
+    private int damage;
 
     public Player(int x, int y, int health, int gold, int armor) {
         this.x = x;
@@ -17,6 +19,8 @@ public class Player {
         this.gold = gold;
         this.armor = armor;
         this.food = 0; // Starting food
+        this.experience = 0;
+        this.damage = 10; // Base damage
     }
 
     public int getX() {
@@ -47,6 +51,14 @@ public class Player {
         return food;
     }
 
+    public int getExperience() {
+        return experience;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
     public void move(int dx, int dy) {
         x += dx;
         y += dy;
@@ -59,5 +71,24 @@ public class Player {
     public void setPosition(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public void takeDamage(int damage) {
+        this.health -= damage;
+    }
+
+    public void addExperience(int experience) {
+        this.experience += experience;
+    }
+
+    public void levelUp() {
+        this.level++;
+        this.health += 20;
+        this.damage += 5;
+        this.experience = 0;
+    }
+
+    public boolean isDead() {
+        return health <= 0;
     }
 }
