8805a63ec8c95579b22d12a6ce9bfd51ecccf6ff
karthikpinneboyina99 <157269250+karthikpinneboyina99@users.noreply.github.com>
2025-03-17 13:25:35 -0500

    Update Player.java

--- ROGUE/src/main/roguegame/Player.java
+++ ROGUE/src/main/roguegame/Player.java
@@ -1,6 +1,9 @@
-package main.roguegame;
+package roguegame;
 
-public class Player {
+import java.util.ArrayList;
+import java.util.List;
+
+class Player {
     private int x;
     private int y;
     private int health;
@@ -10,6 +13,9 @@ public class Player {
     private int food;
     private int experience;
     private int damage;
+    private int hunger;
+    private List<Item> inventory;
+    private int maxInventorySize;
 
     public Player(int x, int y, int health, int gold, int armor) {
         this.x = x;
@@ -21,6 +27,9 @@ public class Player {
         this.food = 0; // Starting food
         this.experience = 0;
         this.damage = 10; // Base damage
+        this.hunger = 100; // Starting hunger level
+        this.inventory = new ArrayList<>();
+        this.maxInventorySize = 10; // Max inventory size
     }
 
     public int getX() {
@@ -59,6 +68,14 @@ public class Player {
         return damage;
     }
 
+    public int getHunger() {
+        return hunger;
+    }
+
+    public List<Item> getInventory() {
+        return inventory;
+    }
+
     public void move(int dx, int dy) {
         x += dx;
         y += dy;
@@ -66,6 +83,10 @@ public class Player {
 
     public void addFood(int amount) {
         this.food += amount;
+    }
+
+    public void addGold(int amount) {
+        this.gold += amount;
     }
 
     public void setPosition(int x, int y) {
@@ -88,7 +109,43 @@ public class Player {
         this.experience = 0;
     }
 
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public void decreaseHunger(int amount) {
+        this.hunger -= amount;
+        if (this.hunger <= 0) {
+            this.health -= 10; // Starvation damage
+            this.hunger = 0;
+        }
+    }
+
+    public void consumeFood() {
+        if (this.food > 0) {
+            this.food--;
+            this.hunger += 20;
+            if (this.hunger > 100) this.hunger = 100;
+        }
+    }
+
     public boolean isDead() {
         return health <= 0;
+    }
+
+    public boolean addItemToInventory(Item item) {
+        if (inventory.size() < maxInventorySize) {
+            inventory.add(item);
+            return true;
+        }
+        return false; // Inventory is full
+    }
+
+    public void dropItem(Item item) {
+        inventory.remove(item);
     }
 }
